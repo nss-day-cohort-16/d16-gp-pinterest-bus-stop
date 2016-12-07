fc5cb29c026feabfd476bf6d3d2145c2f9d3c29d
Zachary Cline <Zcline08@gmail.com>
2016-12-07 10:36:24 -0600

    Delete fb-creds.js

--- App/Value/fb-creds.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-//Make Sure to ignore this file!//
-app.constant("FBCreds", {
-	key: "AIzaSyC5nXUeSojxQID-ntbkWk2GO4CVTwFbyOQ"
-	authDomain: "centrist-94129.firebaseapp.com"
-	URL: "https://centrist-94129.firebaseio.com"
-});
