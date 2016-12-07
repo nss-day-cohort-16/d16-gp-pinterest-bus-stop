2fc6b0db167d5bc1d5dd116495a402d96e8cdc01
Zachary Cline <zcline08@gmail.com>
2016-12-07 10:48:09 -0600

    merged shawns auth.js

--- App/Factories/auth.js
+++ App/Factories/auth.js
@@ -1,43 +1,54 @@
 "use strict";
 
-app.factory("AuthFactory", function(){
-	let currentUser = null;
 
-	// Must Create A User Before We can Use one
+app.auth("authFactory", function () {
+	let currentUser= null;
 
-	let createUser = function (user){
-		let provider = new firebase.auth.GoogleAuthProvider(),
-		currentUser = null;
+let createUser = function () {
 
-		//listen for log in or log out actions and set currentUser's value
-	firebase.auth().onAuthStateChanged(function(user){
-		if (user) {
-			currentUser = user.uid;
-			console.log("currentUser Logged in?", 	currentUser);
-		} else {
-			currentUser = null;
-			console.log("currentUser Logged out?", currentUser);
-		}
+	return firebase.auth().createUserWithEmailandPassword(userObj.)
+}
+
+
+let logIn = function () {
+
+	return firebase.auth().signInWithEmailAndPassword(email, pass);
+}
+
+
+let logOut = function () {
+
+
+	return firebase.auth();
+	signOut();
+}
+
+let isAuthenticated = function (){
+
+	return new Promise ((resolve, reject) => {
+		firebase.auth().onAuthStateChanged((user) => {
+			if (user ) {
+				currentUser = user.uid;
+				resolve (true);
+			} else {
+				resolve (false);
+			}
+		});
+	});
+};
+
+
+let getCurrentUser = function () { 
+
+	return currentUser;
+};
+
+return {
+	createUser,
+	logIn,
+	logOut,
+	isAuthenticated,
+	getCurrentUser
+};
+
 });
-
-	};
-
-	let loginUser =function(user){
-		//
-	};
-
-	let logoutUser = function(user){
-
-	};
-
-	let isAuthenticated = function(user){
-
-	};
-
-	let getUser = function(user){
-
-	};
-
-	return {createUser, loginUser, logoutUser, isAuthenticated, getUser}
-})
-
