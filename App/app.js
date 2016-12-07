f418ec4b29363dfb935f46c81fc1db530851ccb8
Abby Moore <abbyleighanne@gmail.com>
2016-12-07 14:57:57 -0600

    files linked. womp womp.

--- App/app.js
+++ App/app.js
@@ -1,3 +1,48 @@
 "use strict";
-let app = angular.module("Centrist", []);
+let app = angular.module("Centrist", ['ngRoute']);
 
+// Grab the Auth Factory. 
+let isAuth = (AuthFactory) => new Promise((resolve, reject) => {
+	AuthFactory.isAuthenticated()
+		.then((userExists) => {
+			if(userExists){
+				resolve();
+			} else {
+				reject();
+			}
+		});
+});
+
+
+app.config(function($routeProvider){
+	$routeProvider
+		// .when('/', {
+		// 	templateUrl: '' //,
+		// 	//controller: 'ItemListCtrl',
+		// 	// Resolve: safety feature so people can't just go to the URL. 
+		// 	//resolve: {isAuth}
+		// })
+		// .when('/', {
+		// 	templateUrl: '',
+		// 	controller: ""
+		// })
+		.when('/login', {
+			templateUrl: 'partials/loginView.html',
+			controller: "LoginCtrl"
+		})
+		.otherwise('/'); 
+});
+
+
+app.run( ($location, FBCreds) => {
+
+	let creds = FBCreds;
+
+	let authConfig = {
+		apiKey: creds.key,
+		authDomain: creds.authDomain
+	};
+
+	firebase.initializeApp(authConfig);
+
+});
